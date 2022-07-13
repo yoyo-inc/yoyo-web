8ad9ea4dfe467b6db2cd3d6ecbe9a3ad0b6405c6
ypli0629 <liyapeng0629@gmail.com>
2022-07-13 16:34:11 +0800

    feat: add response interceptor

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-// 运行时配置
-
-import { RequestConfig } from '@umijs/max';
+import { history, RequestConfig } from '@umijs/max';
+import { message } from 'antd';
 
 export const request: RequestConfig = {
   baseURL: '/api',
   timeout: 60 * 1000,
+  responseInterceptors: [
+    [
+      (response) => {
+        if (response.status === 401) {
+          history.push('/login');
+        }
+        return response;
+      },
+      (err: any) => {
+        if (err.response.status === 401) {
+          message.error(err.response.data.message);
+          history.push('/login');
+        }
+        return err;
+      },
+    ],
+  ],
 };
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
