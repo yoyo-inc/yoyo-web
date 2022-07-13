3bb3c3e0f068636bd3f4c6e7c8921ce2114d2343
ypli0629 <liyapeng0629@gmail.com>
2022-07-13 21:21:00 +0800

    feat: change layout config

--- config/config.ts
+++ config/config.ts
@@ -3,11 +3,23 @@ import proxy from './proxy';
 import routes from './routes';
 
 export default defineConfig({
+  hash: true,
+  fastRefresh: true,
   antd: {},
   model: {},
   initialState: {},
   request: {},
-  layout: {},
+  layout: {
+    layout: 'mix',
+    menu: {
+      locale: false,
+    },
+    navTheme: 'light',
+    contentWidth: 'Fluid',
+    fixedHeader: false,
+    fixSiderbar: true,
+    colorWeak: false,
+  },
   routes,
   npmClient: 'pnpm',
   proxy,
