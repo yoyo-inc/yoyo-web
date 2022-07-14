ca54bf57cf8d0d98e780b5f8eb1263f5ee8db067
ypli0629 <liyapeng0629@gmail.com>
2022-07-14 11:34:24 +0800

    feat: change swagger doc path

--- openapi.config.js
+++ openapi.config.js
@@ -2,6 +2,6 @@ const { generateService } = require('@umijs/openapi');
 
 generateService({
   requestLibPath: "import { request } from '@umijs/max'",
-  schemaPath: 'http://0.0.0.0:8080/swagger/doc.json',
+  schemaPath: 'http://0.0.0.0:8080/api/swagger/doc.json',
   serversPath: './src/services',
 }).then(() => {});
