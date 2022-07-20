a5f793c62b79b25707fb7f667edc90cba6cce4ad
ypli0629 <liyapeng0629@gmail.com>
2022-07-20 15:31:06 +0800

    feat: layout settings

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,52 @@
 import { history, RequestConfig } from '@umijs/max';
-import { message } from 'antd';
 
 import logo from '@/assets/logo.png';
 import RightContent from '@/components/right-content';
+import api from '@/services/api';
+
+const loginPath = '/login';
 
 export const request: RequestConfig = {
   baseURL: '/api',
   timeout: 60 * 1000,
+  requestInterceptors: [
+    (request) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        request.headers['Authorization'] =
+          'Bearer ' + localStorage.getItem('token');
+      }
+      return request;
+    },
+  ],
   responseInterceptors: [
     [
       (response) => {
-        if (response.status === 401) {
+        if (200 <= response.status && response.status < 300) {
+          return response;
+        } else if (response.status === 401) {
+          localStorage.removeItem('token');
           history.push('/login');
         }
         return response;
-      },
-      (err: any) => {
-        if (err.response.status === 401) {
-          message.error(err.response.data.message);
-          history.push('/login');
-        }
-        return err;
       },
     ],
   ],
 };
 
+export type IInitialState = {
+  currentUser: API.User;
+};
+
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<any> {
-  return { name: 'yoyo-web' };
+  const initialState = {} as IInitialState;
+  if (localStorage.getItem('token')) {
+    const result = await api.user.getUserCurrent({});
+    initialState['currentUser'] = result.data;
+  }
+  return initialState;
 }
 
 export const layout = ({
@@ -37,10 +54,33 @@ export const layout = ({
 }: {
   initialState: { currentUser: API.User };
 }) => {
-  console.log(initialState);
   return {
     logo,
     title: 'yoyo-web',
+    layout: 'mix',
+    menu: {
+      locale: false,
+    },
+    navTheme: 'dark',
+    contentWidth: 'Fluid',
+    fixedHeader: false,
+    fixSiderbar: true,
+    colorWeak: false,
     rightContentRender: () => <RightContent />,
+    onPageChange: () => {
+      if (
+        history.location.pathname !== loginPath &&
+        !initialState?.currentUser
+      ) {
+        history.push(loginPath);
+      }
+    },
+    breadcrumbRender: (routes: any) => [
+      {
+        path: '/',
+        breadcrumbName: '主页',
+      },
+      ...(routes || []),
+    ],
   };
 };
