0db230aec71ddd9b9907481d8d631be896a25394
ypli0629 <liyapeng0629@gmail.com>
2022-07-20 15:32:44 +0800

    feat: add user page

--- src/pages/setting/user/index.tsx
+++ src/pages/setting/user/index.tsx
@@ -1,5 +1,7 @@
 import { ProColumns, ProTable } from '@ant-design/pro-components';
 
+import api from '@/services/api';
+
 export default function User() {
   const columns: ProColumns<API.User>[] = [
     {
@@ -7,10 +9,25 @@ export default function User() {
       title: '用户名',
       onFilter: true,
     },
+    {
+      dataIndex: 'email',
+      title: '邮箱',
+    },
+    {
+      dataIndex: 'phone',
+      title: '手机号',
+    },
   ];
   return (
     <div>
-      <ProTable columns={columns}></ProTable>
+      <ProTable
+        columns={columns}
+        request={async (params) => {
+          return api.user.getUser({
+            ...params,
+          });
+        }}
+      ></ProTable>
     </div>
   );
 }
