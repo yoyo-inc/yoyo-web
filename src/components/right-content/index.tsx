2926cb65e94eb7c41a824084805fecdf35dabe7f
ypli0629 <liyapeng0629@gmail.com>
2022-07-20 15:32:19 +0800

    feat: add RightContent

--- src/components/right-content/index.tsx
+++ src/components/right-content/index.tsx
@@ -1,3 +1,42 @@
+import { useModel } from '@umijs/max';
+import { Avatar, Dropdown, Menu } from 'antd';
+
+import styles from './index.less';
+
 export default function RightContent() {
-  return <div>RightContent</div>;
+  const { initialState } = useModel('@@initialState');
+  const menu = (
+    <Menu
+      items={[
+        // {
+        //   key: "personal",
+        //   label: "个人设置"
+        // },
+        // {
+        //   key: "system",
+        //   label: "系统设置"
+        // },
+        {
+          type: 'divider',
+        },
+        {
+          key: 'logout',
+          label: '退出登录',
+        },
+      ]}
+    ></Menu>
+  );
+  return (
+    <div>
+      <Dropdown overlay={menu} trigger={['click']}>
+        <>
+          <Avatar
+            className={styles.avatar}
+            src={initialState.currentUser.avatar}
+          ></Avatar>
+          <span>{initialState.currentUser.username}</span>
+        </>
+      </Dropdown>
+    </div>
+  );
 }
