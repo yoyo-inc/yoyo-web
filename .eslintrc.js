be07edeebf9105d258e12d15b759caee4e8aed49
ypli0629 <liyapeng0629@gmail.com>
2022-07-20 22:02:44 +0800

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: require.resolve('@umijs/max/eslint'),
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  globals: {
+    page: true,
+    REACT_APP_ENV: true,
+  },
 };
