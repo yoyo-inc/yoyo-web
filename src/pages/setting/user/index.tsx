68b0e255dbf9b0153e791b54e3f9a93c5a067488
ypli0629 <liyapeng0629@gmail.com>
2022-07-22 09:13:39 +0800

    feat: query user

--- src/pages/setting/user/index.tsx
+++ src/pages/setting/user/index.tsx
@@ -1,6 +1,9 @@
-import { ProColumns, ProTable } from '@ant-design/pro-components';
+import React from 'react';
+import type { ProColumns } from '@ant-design/pro-components';
+import { ProTable } from '@ant-design/pro-components';
 
 import api from '@/services/api';
+import { transformPaginatedData } from '@/utils';
 
 export default function User() {
   const columns: ProColumns<API.User>[] = [
@@ -10,6 +13,18 @@ export default function User() {
       onFilter: true,
     },
     {
+      dataIndex: 'sex',
+      title: '性别',
+      valueEnum: {
+        '0': {
+          text: '男',
+        },
+        '1': {
+          text: '女',
+        },
+      },
+    },
+    {
       dataIndex: 'email',
       title: '邮箱',
     },
@@ -17,17 +32,26 @@ export default function User() {
       dataIndex: 'phone',
       title: '手机号',
     },
+    {
+      title: '操作',
+      valueType: 'option',
+      width: 120,
+      render: () => [<a key="edit">编辑</a>, <a key="delete">删除</a>],
+    },
   ];
   return (
     <div>
       <ProTable
         columns={columns}
+        rowKey={'id'}
         request={async (params) => {
-          return api.user.getUser({
-            ...params,
-          });
+          return api.user
+            .getUser({
+              ...params,
+            })
+            .then(transformPaginatedData);
         }}
-      ></ProTable>
+      />
     </div>
   );
 }
