f64b375f59575980aa9edd691c36cea97b67da5f
ypli0629 <liyapeng0629@gmail.com>
2022-11-09 11:19:04 +0800

    feat: remove currentUser after logout

--- src/components/right-content/index.tsx
+++ src/components/right-content/index.tsx
@@ -1,21 +1,39 @@
-import { useModel } from '@umijs/max';
-import { Avatar, Dropdown, Menu } from 'antd';
+import React from 'react';
+import { useModel, history } from '@umijs/max';
+import { Avatar, Button, Dropdown, Menu } from 'antd';
 
 import styles from './index.less';
 
 export default function RightContent() {
-  const { initialState } = useModel('@@initialState');
+  const { initialState, setInitialState } = useModel('@@initialState');
+  const onMenuClick = ({ key }) => {
+    switch (key) {
+      case 'personal':
+        break;
+      case 'system':
+        break;
+      case 'logout':
+        localStorage.removeItem('token');
+        setInitialState((state) => ({
+          ...state,
+          currentUser: {},
+        }));
+        history.push('/login');
+        break;
+    }
+  };
   const menu = (
     <Menu
+      onClick={onMenuClick}
       items={[
-        // {
-        //   key: "personal",
-        //   label: "个人设置"
-        // },
-        // {
-        //   key: "system",
-        //   label: "系统设置"
-        // },
+        {
+          key: 'personal',
+          label: '个人设置',
+        },
+        {
+          key: 'system',
+          label: '系统设置',
+        },
         {
           type: 'divider',
         },
@@ -29,13 +47,10 @@ export default function RightContent() {
   return (
     <div>
       <Dropdown overlay={menu} trigger={['click']}>
-        <>
-          <Avatar
-            className={styles.avatar}
-            src={initialState.currentUser?.avatar}
-          ></Avatar>
-          <span>{initialState.currentUser?.username}</span>
-        </>
+        <div style={{ cursor: 'pointer' }}>
+          <Avatar className={styles.avatar} src={initialState.currentUser?.avatar}></Avatar>
+          <span className={styles.username}>{initialState.currentUser?.username}</span>
+        </div>
       </Dropdown>
     </div>
   );
