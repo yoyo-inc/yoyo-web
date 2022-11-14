f093853f5fcac9cd2f5149e84b8f997e99a56a5f
ypli0629 <liyapeng0629@gmail.com>
2022-11-14 10:31:33 +0800

    feat(components): add TabsMenu component

---
+++ src/components/tabs-menu/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useLocation, history } from '@umijs/max';
+import { Tabs } from 'antd';
+import { Outlet } from '@umijs/max';
+
+export interface TabsMenuItem {
+  key: string;
+  label: string;
+}
+
+export interface TabsMenuProps {
+  commonPath: string;
+  items: TabsMenuItem[];
+  initialActiveKey: string;
+}
+
+export default function TabsMenu(props: TabsMenuProps) {
+  const { commonPath, items, initialActiveKey } = props;
+  const location = useLocation();
+  const typeExp = new RegExp(commonPath + '/(?<type>.*)');
+  const type = typeExp.exec(location.pathname)?.groups?.type;
+  const [activeKey, setActiveKey] = useState(type || initialActiveKey);
+  console.log(activeKey);
+
+  return (
+    <div>
+      <Tabs
+        destroyInactiveTabPane
+        tabPosition="left"
+        activeKey={activeKey}
+        onChange={(key) => {
+          setActiveKey(key);
+          history.push(`${commonPath}/${key}`);
+        }}
+        items={items.map((item) => ({
+          ...item,
+          children: <Outlet />,
+        }))}
+      ></Tabs>
+    </div>
+  );
+}
