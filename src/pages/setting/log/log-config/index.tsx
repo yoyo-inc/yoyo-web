96a3dca4b4b067eb61ba2896938be838967c8669
ypli0629 <liyapeng0629@gmail.com>
2022-12-01 17:05:44 +0800

    feat: limit width

--- src/pages/setting/log/log-config/index.tsx
+++ src/pages/setting/log/log-config/index.tsx
@@ -3,8 +3,8 @@ import { ProForm, ProFormDigit } from '@ant-design/pro-components';
 
 export default function LogConfig() {
   return (
-    <div>
-      <ProForm>
+    <div style={{ width: '640px' }}>
+      <ProForm layout="horizontal" labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
         <ProFormDigit label="日志保留时间"></ProFormDigit>
       </ProForm>
     </div>
