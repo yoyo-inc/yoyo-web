ce6bca3fce15f38b4082f85789be8bc7e5ba8f41
ypli0629 <liyapeng0629@gmail.com>
2022-12-15 10:55:13 +0800

    feat: support auto login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { LoginFormPage, ProFormCheckbox, ProFormText } from '@ant-design/pro-components';
 import { history, request } from '@umijs/max';
 import { useModel } from '@umijs/max';
 import { flushSync } from 'react-dom';
 
-import logo from '@/assets/logo.png';
+import Logo from '@/components/logo';
 import styles from './index.less';
 import api from '@/services/api';
+import backgroundImage from '@/assets/background.png';
+import { getToken } from '@/utils/token';
 
 export default function Login() {
   const { initialState, setInitialState } = useModel('@@initialState');
+  useEffect(() => {
+    if (getToken()) {
+      history.push('/');
+    }
+  }, []);
   return (
     <div className={styles.login}>
       <LoginFormPage
         title={initialState?.systemSetting?.name}
         subTitle={initialState?.systemSetting?.description}
-        backgroundImageUrl="https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png"
-        logo={logo}
+        backgroundImageUrl={backgroundImage}
+        logo={<Logo />}
         onFinish={async (data: any) => {
+          const { autoLogin, ...restData } = data;
           return request('/login', {
             method: 'POST',
-            data,
+            data: restData,
           }).then(async (res) => {
-            localStorage.setItem('token', res.data.token);
+            if (autoLogin) {
+              localStorage.setItem('token', res.data.token);
+            } else {
+              sessionStorage.setItem('token', res.data.token);
+            }
             const [currentUser, currentPermissions] = await Promise.all([
               api.user.getUserCurrent({}),
               api.user.getUserCurrentPermissions(),
