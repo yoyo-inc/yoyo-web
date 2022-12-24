45682cd5290d4b8823028f3be15d5d38a1d41cbf
ypli0629 <liyapeng0629@gmail.com>
2022-12-24 21:29:22 +0800

    feat: add log-config

--- src/pages/setting/log/log-config/index.tsx
+++ src/pages/setting/log/log-config/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ProForm, ProFormDigit } from '@ant-design/pro-components';
+import FormPanel from '@/components/form-panel';
 
 export default function LogConfig() {
   return (
-    <div style={{ width: '640px' }}>
+    <FormPanel>
       <ProForm layout="horizontal" labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
         <ProFormDigit label="日志保留时间"></ProFormDigit>
       </ProForm>
-    </div>
+    </FormPanel>
   );
 }
