730b916b2571ea2dd187d547a2c13aefb12229f5
ypli0629 <liyapeng0629@gmail.com>
2022-12-29 19:30:41 +0800

    feat: add report config

--- src/pages/setting/report/config/index.tsx
+++ src/pages/setting/report/config/index.tsx
@@ -1,19 +1,49 @@
-import { ProForm, ProFormCheckbox, ProFormSelect, ProFormText } from '@ant-design/pro-components';
-import React from 'react';
+import {
+  FormInstance,
+  ProForm,
+  ProFormCheckbox,
+  ProFormSelect,
+  ProFormText,
+} from '@ant-design/pro-components';
+import React, { useEffect, useRef } from 'react';
 import api from '@/services/api';
+import { message } from 'antd';
 
 export default function ReportConfig() {
+  const formRef = useRef<FormInstance>();
+  useEffect(() => {
+    api.report.getReportConfig().then((res) => {
+      formRef.current?.setFields(
+        Object.keys(res.data).map((key) => ({
+          name: key,
+          value: res.data[key],
+        })),
+      );
+    });
+  }, []);
   return (
     <div style={{ width: '640px' }}>
-      <ProForm layout="horizontal" labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
+      <ProForm
+        formRef={formRef}
+        layout="horizontal"
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 18 }}
+        onFinish={async (values) => {
+          return api.report.putReportConfig(values).then((res) => {
+            message.success('更新成功');
+            return res.data;
+          });
+        }}
+      >
         <ProForm.Group title="定期报告设置" direction="vertical">
           <ProFormCheckbox.Group
-            name=""
+            name="period"
             label="周期"
             options={[
               { label: '每天', value: 'day' },
               { label: '每周', value: 'week' },
               { label: '每月', value: 'month' },
+              { label: '每年', value: 'year' },
             ]}
           ></ProFormCheckbox.Group>
           <ProFormSelect
@@ -26,7 +56,6 @@ export default function ReportConfig() {
               return api.report.getReportTypes().then((res) => res.data);
             }}
           ></ProFormSelect>
-          <ProFormText name="reportName" label="报告名称"></ProFormText>
         </ProForm.Group>
       </ProForm>
     </div>
