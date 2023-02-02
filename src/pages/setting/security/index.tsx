e91b705ded5c7486a15891d2dc84917942fb583d
ypli0629 <liyapeng0629@gmail.com>
2023-02-02 14:26:33 +0800

    feat: init system security settings

--- src/pages/setting/security/index.tsx
+++ src/pages/setting/security/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   ProForm,
   ProFormSwitch,
   ProFormDependency,
   ProFormDigit,
   EditableProTable,
+  ProFormInstance,
 } from '@ant-design/pro-components';
 import Page from '@/components/page';
 import FormPanel from '@/components/form-panel';
 import { Checkbox } from 'antd';
+import api from '@/services/api';
+import { expand } from '@/utils';
 
 export default function SystemSecurity() {
+  const formRef = useRef<ProFormInstance>();
+  useEffect(() => {
+    api.systemSecurity.getSystemSecurity().then((res) => {
+      formRef.current?.setFields(expand(res.data));
+    });
+  }, []);
   return (
     <Page>
       <FormPanel>
-        <ProForm layout="horizontal" labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
+        <ProForm
+          formRef={formRef}
+          layout="horizontal"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 18 }}
+        >
           <ProFormSwitch name="forbidRepeatLogin" label="禁止重复登录"></ProFormSwitch>
           <ProFormSwitch name="loginExpireEnable" label="登录过期"></ProFormSwitch>
           <ProFormDependency name={['loginExpireEnable']}>
@@ -38,31 +52,39 @@ export default function SystemSecurity() {
                 return;
               }
               return (
-                <EditableProTable
-                  loading={false}
-                  rowKey="id"
-                  name="loginIPWhitelist"
-                  controlled
-                  columns={[
-                    {
-                      title: '启用',
-                      dataIndex: 'enable',
-                      width: 60,
-                      valueType: 'checkbox',
-                      formItemProps: { valuePropName: 'checked' },
-                      renderFormItem() {
-                        return <Checkbox></Checkbox>;
+                <ProForm.Item label="白名单列表">
+                  <EditableProTable
+                    loading={false}
+                    rowKey="id"
+                    name="loginIPWhitelist"
+                    controlled
+                    columns={[
+                      {
+                        title: '启用',
+                        dataIndex: 'enable',
+                        width: 60,
+                        valueType: 'checkbox',
+                        formItemProps: { valuePropName: 'checked' },
+                        renderFormItem() {
+                          return <Checkbox></Checkbox>;
+                        },
+                        render(_: any, entity: any) {
+                          return <Checkbox checked={entity.enable} />;
+                        },
                       },
-                      render(_: any, entity: any) {
-                        return <Checkbox checked={entity.enable} />;
+                      {
+                        title: 'IP',
+                        dataIndex: 'ip',
                       },
-                    },
-                    {
-                      title: 'IP',
-                      dataIndex: 'ip',
-                    },
-                  ]}
-                ></EditableProTable>
+                    ]}
+                    recordCreatorProps={{
+                      record(index) {
+                        return { id: index + 1, enable: true };
+                      },
+                      creatorButtonText: '新增IP',
+                    }}
+                  ></EditableProTable>
+                </ProForm.Item>
               );
             }}
           </ProFormDependency>
