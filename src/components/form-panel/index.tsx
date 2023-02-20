5f0b41452d9d70b2dea5356695829cd22ff91a17
ypli0629 <liyapeng0629@gmail.com>
2023-02-20 14:10:13 +0800

    fix: type

--- src/components/form-panel/index.tsx
+++ src/components/form-panel/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function FormPanel(props) {
+export default function FormPanel(props: React.PropsWithChildren<any>) {
   return <div style={{ width: '640px' }}>{props.children}</div>;
 }
